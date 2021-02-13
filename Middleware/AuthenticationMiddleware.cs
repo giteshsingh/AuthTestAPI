423b76663b187f27ced85e8b077e76b747355a6f
Gitesh <giteshsingh17@gmail.com>
2021-02-13 14:47:44 +0530

    Create AuthenticationMiddleware.cs
    added new class

---
+++ Middleware/AuthenticationMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthTestAPI.Middleware
+{
+    public class AuthenticationMiddleware
+    {
+    }
+}
